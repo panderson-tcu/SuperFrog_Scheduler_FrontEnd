b16242ebfefb2a0d10fe48d8a65b518f91f8f03f
Paige Anderson <panderson.pdx@gmail.com>
2023-04-21 19:39:40 -0500

    landing page

--- superfrog-scheduler-frontend/src/main.js
+++ superfrog-scheduler-frontend/src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import FontAwesomeIcon from "./fontawesome-icons.js";
 import './assets/main.css'
 
+
+
 const app = createApp(App)
+    .component("font-awesome-icon", FontAwesomeIcon)
+    .use(router)
+    .mount('#app');
 
-app.use(router)
-
-app.mount('#app')
