5526586124426fc2392a6e8f2ba4b40f85c6448d
annajgadbois <113142203+annajgadbois@users.noreply.github.com>
2023-04-26 15:06:24 -0500

    Anna Changes

--- superfrog-scheduler-frontend/src/fontawesome-icons.js
+++ superfrog-scheduler-frontend/src/fontawesome-icons.js
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faPhone, faUser, faFlag, faMoneyBill1 , faLocationDot, faCalendarDays, faPencil, faAddressBook, faCircleChevronDown, faCircleChevronUp, faBarsProgress, faBook, faBell, faB} from "@fortawesome/free-solid-svg-icons";
+import { faPhone, faUser, faFlag, faMoneyBill1 , faLocationDot, faCalendarDays, faPencil, faAddressBook, faCircleChevronDown, faCircleChevronUp, faBarsProgress, faBook, faBell, faArrowLeftLong, faArrowRightLong, faHouse} from "@fortawesome/free-solid-svg-icons";
 
-library.add(faPhone, faUser, faFlag, faMoneyBill1, faLocationDot, faCalendarDays, faPencil, faAddressBook, faCircleChevronDown, faCircleChevronUp, faBarsProgress, faBook, faBell);
+library.add(faPhone, faUser, faFlag, faMoneyBill1, faLocationDot, faCalendarDays, faPencil, faAddressBook, faCircleChevronDown, faCircleChevronUp, faBarsProgress, faBook, faBell, faArrowLeftLong, faArrowRightLong, faHouse);
 
 export default FontAwesomeIcon;
